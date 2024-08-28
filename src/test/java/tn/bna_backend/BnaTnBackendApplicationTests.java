3837aeae1a96ca485a9c6386705424d508514e59
fathallah-ahmed <ahmed.fathallah@esprit.tn>
2024-08-28 22:35:50 +0100

    setup

---
+++ src/test/java/tn/bna_backend/BnaTnBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.bna_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BnaTnBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
